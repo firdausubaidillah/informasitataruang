504b9358188448f1c3521c3f64f088065ec1d6a4
Tsamrotul Fuad <tsamrotulfuad@live.com>
2019-07-08 14:01:56 +0700

    Edit lagi

--- application/models/M_pemohon.php
+++ application/models/M_pemohon.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_pemohon extends CI_Model {
 
-    var $table = 't_sekolah';
-    var $primary = 'id_sekolah';
+    var $table = 'tbl_pemohon';
+    var $primary = 'noreg';
 
     public function __construct()
     {
@@ -14,7 +14,7 @@ class M_pemohon extends CI_Model {
 
     public function tampil()
     {
-        $query = $this->db->query("SELECT * FROM t_sekolah");
+        $query = $this->db->query("SELECT * FROM tbl_pemohon");
         return $query;
     }
 
