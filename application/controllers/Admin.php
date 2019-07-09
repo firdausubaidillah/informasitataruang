9e89a7642feb00f93411163e0c33efe249d4eba5
Tsamrotul Fuad <tsamrotulfuad@live.com>
2019-07-09 09:57:48 +0700

    Edit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends CI_Controller {
 		$this->load->helper('url');
 		$this->load->model('M_pemohon');
 	}
+
 	public function index()
 	{
 		$data['title'] = 'Dashboard';
@@ -22,6 +23,69 @@ class Admin extends CI_Controller {
 		$data['page']  = '_backend/pemohon/index';
 		$data['data'] = $this->M_pemohon->tampil()->result();
 		$this->load->view('_backend/index', $data);
+	}
+
+	public function tambah_pemohon()
+	{
+		$data['title'] = 'Pemohon';
+		$data['page'] = '_backend/pemohon/tambah';
+		$this->load->view('_backend/index', $data);
+	}
+
+	function tambahdata_pemohon()
+	{
+		if (isset($_POST['submit'])) {
+			$object = array('nama'			=> $this->input->post('nama'),
+							'jk'			=> $this->input->post('jk'),
+							'alamat'		=> $this->input->post('alamat'),
+							'tgl'			=> $this->input->post('tgl'),
+							'pekerjaan'		=> $this->input->post('pekerjaan'),
+							'pemanfaatan'	=> $this->input->post('pemanfaatan'),
+							'lokasi'		=> $this->input->post('lokasi'),
+							'notelp'		=> $this->input->post('notelp')
+						);
+		
+			$object = $this->security->xss_clean($object);
+			$this->M_pemohon->simpan($object);
+			redirect('admin/pemohon');
+		} else {
+			redirect('admin/pemohon');
+		}
+	}
+
+	public function tampilubah_pemohon($id)
+	{
+		$data['title']	= 'Pemohon';
+		$data['page']	= '_backend/pemohon/ubah';
+		$data['data']	= $this->M_pemohon->tampil_ubah($id)->result();
+		$this->load->view('_backend/index', $data);
+	}
+
+	function ubahdata_pemohon(Type $var = null)
+	{
+		if(isset($_POST['submit'])){
+			$id 	= $this->input->post('noreg');
+			$object = array('nama' 			=> $this->input->post('nama'),
+							'jk'			=> $this->input->post('jk'),
+							'alamat'		=> $this->input->post('alamat'),
+							'tgl'			=> $this->input->post('tgl'),
+							'pekerjaan'		=> $this->input->post('pekerjaan'),
+							'pemanfaatan'	=> $this->input->post('pemanfaatan'),
+							'lokasi'		=> $this->input->post('lokasi'),
+							'notelp'		=> $this->input->post('notelp'),
+							);
+			$this->M_pemohon->ubah($id, $object);
+			$object = $this->security->xss_clean($object);
+			redirect('admin/pemohon');
+		} else {
+			redirect('admin/pemohon');
+		}
+	}
+
+	function hapus_pemohon($id)
+	{
+		$this->M_pemohon->hapus($id);
+		redirect('admin/pemohon');
 	}
 
 }
