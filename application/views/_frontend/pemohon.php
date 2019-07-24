7401aa822f7642aa2a46e22d612feddbe2c679f7
Tsamrotul Fuad <tsamrotulfuad@live.com>
2019-07-24 14:18:02 +0700

    u

--- application/views/_frontend/pemohon.php
+++ application/views/_frontend/pemohon.php
@@ -48,47 +48,256 @@
             <i class="fa fa-bars"></i>
           </button>
       </div>
-      <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- User Account Menu -->
-            <li class="dropdown user user-menu">
-              <!-- Menu Toggle Button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <!-- The user image in the navbar-->
-                <img src="<?= base_url("assets/backend/"); ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                <span class="hidden-xs"><?= $nama; ?></span>
-              </a>
-              <ul class="dropdown-menu">
-                <!-- The user image in the menu -->
-                <li class="user-header">
-                  <img src="<?= base_url("assets/backend/"); ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                  <p>
-                    <?= $nama; ?>
-                    <small>Member since Nov. 2012</small>
-                  </p>
-                </li>
-                <!-- Menu Body -->
-                
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                  <div class="pull-left">
-                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-                  </div>
-                  <div class="pull-right">
-                    <a href="<?= base_url('login/logout_pemohon');?>" class="btn btn-default btn-flat">Sign out</a>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      <!-- /.container-fluid -->
     </nav>
   </header>
   <!-- Full Width Column -->
   <div class="content-wrapper">
+
+  <section class="content">
+
+      <div class="row">
+        <div class="col-md-3">
+
+          <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="<?= base_url("assets/backend/"); ?>dist/img/user4-128x128.jpg" alt="User profile picture">
+
+              <h3 class="profile-username text-center"><?= $nama; ?></h3>
+
+              <p class="text-muted text-center">Pemohon</p>
+
+              <ul class="list-group list-group-unbordered">
+                <li class="list-group-item">
+                  <b>Followers</b> <a class="pull-right">1,322</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Following</b> <a class="pull-right">543</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Friends</b> <a class="pull-right">13,287</a>
+                </li>
+              </ul>
+
+              <a href="<?= base_url('login/logout_pemohon');?>" class="btn btn-danger btn-block"><b>Logout</b></a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-9">
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+              <li class="active"><a href="#pemohon" data-toggle="tab">Data Pemohon</a></li>
+              <li><a href="#berkas" data-toggle="tab">Berkas</a></li>
+              <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
+
+            </ul>
+            <div class="tab-content">
+              <div class="tab-pane">
+                <!-- /.post -->
+              </div>
+              <!-- /.tab-pane -->
+              <div class="active tab-pane" id="pemohon">
+              <form method="post" action="<?= base_url("pemohon/tambahdatapemohon"); ?>" enctype="multipart/form-data" class="form-horizontal">
+                  <div class="form-group">
+                    <label for="inputNoreg" class="col-sm-2 control-label">No. Reg</label>
+                    
+                    <div class="col-sm-10">
+                    <input type="text" name="noreg" class="form-control" id="noreg" value="<?= $kodeunik; ?>" readonly>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputNama" class="col-sm-2 control-label">Nama Lengkap</label>
+
+                    <div class="col-sm-10">
+                    <input type="text" name="nama" class="form-control" id="inputnama" placeholder="Nama" value="<?= $nama; ?>">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputJK" class="col-sm-2 control-label">Jenis Kelamin</label>
+
+                    <div class="col-sm-10">
+                       <select class="form-control" name="jk">
+                          <option value="">--- Pilih jenis Kelamin ---</option> 
+                          <option>Laki-laki</option>
+                          <option>Perempuan</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputAlamat" class="col-sm-2 control-label">Alamat Lengkap</label>
+
+                    <div class="col-sm-10">
+                      <textArea name="alamat" class="form-control" id="inputalamat" placeholder="Alamat Lengkap"></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputTgl" class="col-sm-2 control-label">Tanggal</label>
+
+                    <div class="col-sm-10">
+                      <input type="date" name="tgl" class="form-control" id="inputtanggal" placeholder="Tanggal Pengajuan">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPekerjaan" class="col-sm-2 control-label">Pekerjaan</label>
+
+                    <div class="col-sm-10">
+                    <input type="text" name="pekerjaan" class="form-control" id="inputpekerjaan" placeholder="Pekerjaan">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPemanfaatan" class="col-sm-2 control-label">Pemanfaatan</label>
+
+                    <div class="col-sm-10">
+                    <input type="text" name="pemanfaatan" class="form-control" id="inputpemanfaatan" placeholder="Pemanfaatan">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputLokasi" class="col-sm-2 control-label">Lokasi yang diajukan</label>
+
+                    <div class="col-sm-10">
+                    <input type="text" name="lokasi" class="form-control" id="inputlokasi" placeholder="Alamat Lokasi yang diajukan">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputHp" class="col-sm-2 control-label">No. Telpon/HP</label>
+
+                    <div class="col-sm-10">
+                    <input type="text" name="notelp" class="form-control" id="inputnotelpon" placeholder="No. Telpon/HP" value="<?= $notelp; ?>">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="timeline">
+                <!-- The timeline -->
+                <ul class="timeline timeline-inverse">
+                  <!-- timeline time label -->
+                  <li class="time-label">
+                        <span class="bg-red">
+                          10 Feb. 2014
+                        </span>
+                  </li>
+                  <!-- /.timeline-label -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-envelope bg-blue"></i>
+
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+
+                      <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                      <div class="timeline-body">
+                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                        weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                        jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                        quora plaxo ideeli hulu weebly balihoo...
+                      </div>
+                      <div class="timeline-footer">
+                        <a class="btn btn-primary btn-xs">Read more</a>
+                        <a class="btn btn-danger btn-xs">Delete</a>
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-user bg-aqua"></i>
+
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+
+                      <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
+                      </h3>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-comments bg-yellow"></i>
+
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
+
+                      <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                      <div class="timeline-body">
+                        Take me to your leader!
+                        Switzerland is small and neutral!
+                        We are more like Germany, ambitious and misunderstood!
+                      </div>
+                      <div class="timeline-footer">
+                        <a class="btn btn-warning btn-flat btn-xs">View comment</a>
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <!-- timeline time label -->
+                  <li class="time-label">
+                        <span class="bg-green">
+                          3 Jan. 2014
+                        </span>
+                  </li>
+                  <!-- /.timeline-label -->
+                  <!-- timeline item -->
+                  <li>
+                    <i class="fa fa-camera bg-purple"></i>
+
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+
+                      <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                      <div class="timeline-body">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                        <img src="http://placehold.it/150x100" alt="..." class="margin">
+                      </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                  <li>
+                    <i class="fa fa-clock-o bg-gray"></i>
+                  </li>
+                </ul>
+              </div>
+              <!-- /.tab-pane -->
+
+             
+            </div>
+            <!-- /.tab-content -->
+          </div>
+          <!-- /.nav-tabs-custom -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+    </section>
+
+
+
+
+
   <section class="content">
       <div class="row">
         <!-- left column -->
@@ -178,7 +387,6 @@
       </div>
       <!-- /.row -->
     </section>
-      <!-- /.content -->
     </div>
     <!-- /.container -->
   </div>
